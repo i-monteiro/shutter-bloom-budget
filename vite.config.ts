9755690f59935340e5af2de92f40bba8b4dd54b6
i-monteiro <italo.dsm6@gmail.com>
2025-04-21 13:33:56 -0300

    alterando vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: [
+      "c4cc0ed0-8577-4945-af5a-7c77b4e312a9.lovableproject.com"
+    ]
   },
   plugins: [
     react(),
